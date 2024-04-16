3775d2ed1763a93bfb1bd3870bb5955085ec940f
Bardiashams <bardiasham1375@gmail.com>
2024-04-16 09:52:01 +0330

    salam5

--- danial.js
+++ danial.js
@@ -1,1 +1,25 @@
-<div>danial</div>
+import React from "react";
+
+function danial() {
+  return (
+    <div>
+      
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+      <div>danial</div>
+    </div>
+  );
+}
+
+export default danial;

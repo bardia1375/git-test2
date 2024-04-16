e4216d9609837aa558ed82da88333fb2d8f7b373
Bardiashams <bardiasham1375@gmail.com>
2024-04-16 10:54:04 +0330

    بلابلابلا

--- danial.js
+++ danial.js
@@ -6,13 +6,9 @@ function danial() {
   return (
     <div>
       {" "}
-<<<<<<< HEAD
       <div>danials</div>
       <div>sdfsdf</div>
 
-=======
-      <div>danial</div>
->>>>>>> 21d05c27ddd3b97f5250b804e3fd869aa146da54
     </div>
   );
 }
